c9e8702dfaa842797dbcb5e66473ef0772561670
duvan1214 <duvantaboada@gmail.com>
2026-05-15 21:09:15 -0500

    Add files via upload

--- Producto.java
+++ Producto.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 /**
- *
  * @author UNICORDOBA
  */
 public class Producto {
@@ -15,6 +10,19 @@ public class Producto {
     private int stock;
     private String especificacion;
     private String imagen;
+
+    public Producto(String nombre, double precio, int stock, String especificacion, String imagen) {
+       
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+        this.especificacion = especificacion;
+        this.imagen = imagen;
+    }
+
+    public Producto(String nom, String pre) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
     public String getNombre() {
         return nombre;
@@ -55,5 +63,4 @@ public class Producto {
     public void setImagen(String imagen) {
         this.imagen = imagen;
     }
-    
 }
